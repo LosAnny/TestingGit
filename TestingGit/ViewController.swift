b7c8686d0ec5cf78d1f798ab01c3f278e8f71b62
DimkaVinka <dimkavinka@gmail.com>
2022-07-14 21:56:38 +0300

    some
    # Conflicts:
    #	.gitignore

--- TestingGit/ViewController.swift
+++ TestingGit/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  TestingGit
-//
-//  Created by Анна Лошакова on 14.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
